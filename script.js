ae5acd59f5e730e5c42fe615ec9632af40c35fb3
AbeAsseged <abenezerdaniel@live.com>
2023-07-26 22:22:31 -0500

    third commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const flagQuestions = [
+    {
+      correctFlag: "flagImg.jpg",
+      choices: [
+        {
+            answer: "Germany",
+            isCorrect: false
+        },
+        {
+            answer: "Spain",
+            isCorrect: false
+        },
+        {
+            answer: "Austria",
+            isCorrect: true
+        },
+      ]
+    },
+    {
+      correctFlag: "flagImg.jpg",
+      choices: [
+        {
+            answer: "Germany",
+            isCorrect: false
+        },
+        {
+            answer: "Spain",
+            isCorrect: false
+        },
+        {
+            answer: "Austria",
+            isCorrect: true
+        },
+      ]
+    }
+  ]
