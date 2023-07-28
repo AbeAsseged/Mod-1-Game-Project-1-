b8a27365f5f65a138ca6b40c8577c6a6320ecd90
AbeAsseged <abenezerdaniel@live.com>
2023-07-28 01:23:52 -0500

    commit

---
+++ README.md
@@ -0,0 +1,5 @@
+google fonts , asking countinant, capital city, historical question , what countries does it border, fun facts, recent news , governemtn system , flag color meanings, national dishes and sports, traditional clothing and mannerizims 
+
+slovina serbia 
+hungry nicragua guatamala 
+rominia moldova ecuador Anodora Malta uzpakistan pakistan jordan uae tajakistan kuwait Christmas Island aruba 
--- script.js
+++ script.js
@@ -1,36 +1,182 @@
 const flagQuestions = [
     {
-      correctFlag: "flagImg.jpg",
+      correctFlag: "Q1-1.jpg",
       choices: [
         {
-            answer: "Germany",
+            answer: "Curacao",
             isCorrect: false
         },
         {
-            answer: "Spain",
+            answer: "Grenada",
             isCorrect: false
         },
         {
-            answer: "Austria",
+            answer: "Guyana",
             isCorrect: true
         },
       ]
     },
     {
-      correctFlag: "flagImg.jpg",
+      correctFlag: "Q 2.jpg",
       choices: [
         {
-            answer: "Germany",
-            isCorrect: false
-        },
-        {
-            answer: "Spain",
-            isCorrect: false
-        },
-        {
-            answer: "Austria",
+            answer: "Brunei",
             isCorrect: true
+        },
+        {
+            answer: "Borneo",
+            isCorrect: false
+        },
+        {
+            answer: "Guam",
+            isCorrect: false
         },
       ]
     }
   ]
+
+    {
+      correctFlag: "Q 3.png",
+      choices: [
+        {
+            answer: "Australia",
+            isCorrect: false
+        },
+        {
+            answer: "Cook Islands",
+            isCorrect: false
+        },
+        {
+            answer: "New Zealand",
+            isCorrect: true
+        },
+      ]
+    },
+
+      {
+        correctFlag: "Q 4.png",
+        choices: [
+          {
+              answer: "Nicaragua",
+              isCorrect: false
+          },
+          {
+              answer: "San Marino",
+              isCorrect: true
+          },
+          {
+              answer: "Estonia",
+              isCorrect: false
+          },
+        ]
+      },
+
+        {
+          correctFlag: "Q 5.png",
+          choices: [
+            {
+                answer: "Belarus",
+                isCorrect: false
+            },
+            {
+                answer: "Burundi",
+                isCorrect: false
+            },
+            {
+                answer: "Bulgaria",
+                isCorrect: true
+            },
+          ]
+        },
+
+        {
+          correctFlag: "Q 6.jpeg",
+          choices: [
+            {
+              answer: "Chad",
+              isCorrect: true
+            },
+            {
+              answer: "Romania",
+              isCorrect: false
+            },
+            {
+              answer: "Andorra",
+              isCorrect: false
+            },
+          ]
+        },
+
+        {
+          correctFlag: "Q 7.png",
+          choices: [
+            {
+              answer: "Togo",
+              isCorrect: true
+            },
+            {
+              answer: "Panama",
+              isCorrect: false
+            },
+            {
+              answer: "Lithuania",
+              isCorrect: false
+            },
+          ]
+        },
+
+        {
+          correctFlag: "Q 8.png",
+          choices: [
+            {
+              answer: "Algeria",
+              isCorrect: false
+            },
+            {
+              answer: "Turkmenistan",
+              isCorrect: true
+            },
+            {
+              answer: "Pakistan",
+              isCorrect: false
+            },
+          ]
+        },
+
+        {
+          correctFlag: "Q 9.png",
+          choices: [
+            {
+                answer: "Oman",
+                isCorrect: true
+            },
+            {
+                answer: "Madagascar",
+                isCorrect: false
+            },
+            {
+                answer: "UAE",
+                isCorrect: false
+            },
+          ]
+        },
+
+        {
+          correctFlag: "Q 10.jpg",
+          choices: [
+            {
+                answer: "Suriname",
+                isCorrect: false
+            },
+            {
+                answer: "Dominica",
+                isCorrect: true
+            },
+            {
+                answer: "Jamaica",
+                isCorrect: false
+            },
+          ]
+        },
+             
+
